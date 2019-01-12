7559bdf3b7d7ad56b28e77aadcdfa94ff0136581
JohnnySu <Johnny.wySu@sjtu.edu.cn>
2019-01-12 23:32:46 +0800

    first commit,add 1_easy_TwoSum.py

--- 1_easy_TwoSum.py
+++ 1_easy_TwoSum.py
@@ -6,3 +6,9 @@ class Solution:
             if target - value in dictMap:
                 return dictMap[target - value], index
             dictMap[value] = index
+
+if __name__=='__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    s = Solution
+    print(s.twoSum(s,nums,target)) #(0, 1)
