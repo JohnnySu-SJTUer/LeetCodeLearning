46750a727a289feb2a37a6cfe6004df81d526d7f
JohnnySu <Johnny.wySu@sjtu.edu.cn>
2019-01-13 11:21:13 +0800

    first commit,add 1_easy_TwoSum.py

---
+++ 108_easy_sortedArrayToBST.py
@@ -0,0 +1,46 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if not nums:
+            return None
+        mid = len(nums)//2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(self, nums[:mid])
+        root.right = self.sortedArrayToBST(self, nums[mid+1:])
+        return root
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+if __name__=='__main__':
+    s = Solution
+    nums = [-10,-3,0,5,9]
+    ret = s.sortedArrayToBST(s, nums)
+    out = treeNodeToString(ret)
+    print(out)
