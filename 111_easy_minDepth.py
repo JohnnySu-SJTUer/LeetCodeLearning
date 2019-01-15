e17c1e9aefb8b9ad13bd4609cde61139a2a8e19d
JohnnySu <Johnny.wySu@sjtu.edu.cn>
2019-01-15 11:16:07 +0800

    add, commit and push 111_easy_minDepth.py

---
+++ 111_easy_minDepth.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        :BFS iteration
+        """
+        if not root:
+            return 0
+        if root.left==None or root.right==None:
+            return self.minDepth(root.left)+self.minDepth(root.right)+1
+        return min(self.minDepth(root.right),self.minDepth(root.left))+1
