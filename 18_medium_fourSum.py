0aedf8b1c147cb5802da121bb7ec1b19629751eb
JohnnySu <Johnny.wySu@sjtu.edu.cn>
2019-03-18 23:18:50 +0800

    add, commit and push 18.py

---
+++ 18_medium_fourSum.py
@@ -0,0 +1,34 @@
+class Solution:
+    def fourSum(self,nums,target):
+        if len(nums)<4:
+            return []
+        nums.sort()
+        res = []
+        for i in range(len(nums)-3):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1,len(nums)-2):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                left,right=j+1,len(nums)-1
+                while left<right:
+                    sum=nums[i]+nums[j]+nums[left]+nums[right]
+                    if sum==target:
+                        res.append([nums[i],nums[j],nums[left],nums[right]])
+                        while left<right and nums[left]==nums[left+1]:
+                            left+=1
+                        while left<right and nums[right]==nums[right-1]:
+                            right-=1
+                        left+=1
+                        right-=1
+                    elif sum<target:
+                        left+=1
+                    else:
+                        right-=1
+        return res
+
+
+if __name__=='__main__':
+    nums = [-3,-1,0,2,4,5]
+    s = Solution()
+    print(s.fourSum(nums,0))
